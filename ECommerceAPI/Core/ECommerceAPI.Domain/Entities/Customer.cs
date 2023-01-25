ec3f4034d95cbfd5f5294cd2fb833788208393d7
İsmail Emre Güngör <ismailemre.gungor@gazi.edu.tr>
2023-01-25 16:06:51 +0300

    add Customer, Order, Product entity objects

---
+++ ECommerceAPI/Core/ECommerceAPI.Domain/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Domain.Entities
+{
+    public class Customer: BaseEntitiy
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Order> Orders { get; set; } = new List<Order>();
+    }
+}
