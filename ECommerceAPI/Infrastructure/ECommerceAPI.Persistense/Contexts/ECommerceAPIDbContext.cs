c90644db1a2aeac07f4ac59fb956ff5dca9584ae
İsmail Emre Güngör <ismailemre.gungor@gazi.edu.tr>
2023-01-27 02:45:14 +0300

    create and migrate database design, add connection string configuration

--- ECommerceAPI/Infrastructure/ECommerceAPI.Persistense/Contexts/ECommerceAPIDbContext.cs
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistense/Contexts/ECommerceAPIDbContext.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ECommerceAPI.Persistense.Contexts
+namespace ECommerceAPI.Persistence.Contexts
 {
     public class ECommerceAPIDbContext: DbContext
     {
---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistense/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceAPI.Persistence.Contexts;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+        }
+    }
+}
